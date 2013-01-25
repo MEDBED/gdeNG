eeeecada61430db568da6e1da60b1323bc36e488
Guillaume Vernet <git@g-d-e.eu>
2013-01-25 13:09:33 +0100

    Revert "initial commit"
    This reverts commit 5bcda37f99db805dd78ec592ffae6783fe188cec.

--- include/textes.php
+++
@@ -1,5 +0,0 @@
-<?php
-mb_internal_encoding("UTF-8");
-$GLOBALS['textes']['mail']['PS'] = "\n\nP.S : Ne répondez pas à cet e-mail. Il a été généré automatiquement pour votre information !";
-$GLOBALS['textes']['mail']['sign'] = "\n\n--\n".$GLOBALS['params']['appli']['title_appli']."\nWeb : ".$GLOBALS['params']['appli']['url'];
-?>
